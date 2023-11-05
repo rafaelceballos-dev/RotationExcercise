c4be3ad4e130c5453b7ad2383cdf0519139c54b7
Rafael Ceballlos <rafael.ceballos@xibalbalabs.com>
2023-11-04 22:55:57 -0600

    implementation

--- src/RotationExcers/Program.cs
+++ src/RotationExcers/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RotationExcers;
+try
+{
+
+Console.WriteLine("Write a list of numbers");
+var strNumbers = Console.ReadLine();
+
+Console.WriteLine("Write how many times you want it to turn left:");
+var strTimes = Console.ReadLine();
+var intTimes = int.Parse(strTimes);
+
+var rotative = new Rotative();
+
+strNumbers.ToList().ForEach(i => {  
+    rotative.Add(int.Parse(i.ToString())); 
+});
+
+rotative.RotLeft(intTimes);
+
+Console.WriteLine(rotative.PrintElements());
+
+Console.WriteLine();
+
+}
+catch
+{
+    Console.WriteLine("Something went wrong.");
+}
---
+++ src/RotationExcers/Rotative.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RotationExcers
+{
+    internal class Rotative
+    {
+        public List<int> elements = new List<int>();
+
+        public void Add(int element)
+        {
+            elements.Add(element);
+        }
+        public void RotLeft(int number) 
+        { 
+            for(int i = 0; i < number; i++)
+            {
+                Rotate();
+            }
+        }
+
+        internal string PrintElements()
+        {
+            var result = new StringBuilder(string.Empty);
+            elements.ForEach(i =>
+            {
+                result.Append($"{i} ");
+            });
+            
+            return result.ToString();
+        }
+
+        private void Rotate()
+        {
+            var firstNum = elements.First();
+            elements.RemoveAt(0);
+            elements.Add(firstNum);
+        }
+    }
+}
